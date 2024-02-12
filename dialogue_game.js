bca89105eb2e3678e43e7352610b2558b8a9b844
anurni <anninieminen23@gmail.com>
2024-02-12 13:50:59 +0100

    typo

--- dialogue_game.js
+++ dialogue_game.js
@@ -78,7 +78,7 @@ const dialogueGame = setup({
         }
     },
 
-    ListenGreeting: {
+    listenGreeting: {
     entry: "listenForUsersAnswer",
     on: {
         RECOGNISED: "SayInstructions"
