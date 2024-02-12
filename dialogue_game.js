e9aa4812798d4e5de0d90f7a1d12acab0f37e305
Anurni <150805349+Anurni@users.noreply.github.com>
2024-02-12 13:49:30 +0100

    added some basic things from lab2 (DM)

--- dialogue_game.js
+++ dialogue_game.js
@@ -1,2 +1,89 @@
-// our code will be here
-//testing the syncronization
+import { assign, createActor, setup } from "xstate";
+import { speechstate } from "speechstate";
+import { createBrowserInspector } from "@statelyai/inspect";
+import { KEY } from "./azure.js"; //whose key should we be using?
+
+
+const inspector = createBrowserInspector();
+
+const azureCredentials = {
+  endpoint:
+    "https://northeurope.api.cognitive.microsoft.com/sts/v1.0/issuetoken",
+  key: KEY, //whose key??
+};
+
+// our settings:
+const settings = {
+  azureCredentials: azureCredentials,
+  asrDefaultCompleteTimeout: 0,
+  asrDefaultNoInputTimeout: 5000,
+  locale: "en-US",
+  ttsDefaultVoice: "en-US-JaneNeural",
+};
+
+// our grammar:
+const grammar = {
+  agreeWords: ["yes","yup","of course","yeah", "absolutely", "it will", "yes please"],
+  disagreewords: ["no","nope","nah", "no thanks"]
+};
+
+// our functions:
+function isInGrammar(utterance) {
+    return (utterance.toLowerCase() in grammar);
+  }
+
+//creating the machine:
+const dialogueGame = setup({
+    actions: {
+      listenForUsersAnswer : ({ context }) => 
+      context.ssRef.send({
+         type: "LISTEN" }),
+  
+      speakToTheUser : ({ context}, params) => 
+      context.ssRef.send({
+         type: "SPEAK",
+        value: {
+          utterance: params
+        },
+      }),
+  
+      //add assign action
+  
+  }}).createMachine({
+    
+    context: {
+    something_1: '',
+    something_2: '',
+    something_3: '',
+  },
+
+  id: "dialogueGame",
+  initial: "Prepare",
+  states: {
+    Prepare: {
+        entry: [
+        assign({
+        ssRef: ({ spawn }) => spawn(speechstate, { input: settings }),
+            }),
+            ({ context }) => context.ssRef.send({ type: "PREPARE" }),
+            ],
+        on: { 
+            ASRTTS_READY: "WaitToStart" },
+    },
+
+  SayGreeting: {
+    entry: [{type: "speakToTheUser", params: "Welcome to the game! What is your name?"}],
+    on: {
+        SPEAK_COMPLETE: "listenGreeting"
+        }
+    },
+
+    ListenGreeting: {
+    entry: "listenForUsersAnswer",
+    on: {
+        RECOGNISED: "SayInstructions"
+    },
+    },
+    },
+  })
+
